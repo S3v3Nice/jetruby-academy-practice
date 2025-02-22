b34474aa6abd45a871f7d679eb040831ff68cca4
S3v3N <s3v3nicee@gmail.com>
2025-02-22 21:24:14 +0300

    lesson-7: Add controllers; Add views for Dashboard and Users

---
+++ lesson-7-rails-lab-reports/app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update destroy]
+
+  def index
+    @users = User.includes(:lab_reports).order(created_at: :desc).all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: "User was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.lab_reports.exists?
+      redirect_to users_path, alert: "Can't delete user with lab reports."
+    else
+      @user.destroy
+      redirect_to users_path, notice: "User was successfully deleted."
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+end
--- lesson-7-rails-lab-reports/config/routes.rb
+++ lesson-7-rails-lab-reports/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "pages/index"
+  get "dashboard/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: "pages#index"
+  root to: "dashboard#index"
+
+  resources :lab_reports
+  resources :users
 end
