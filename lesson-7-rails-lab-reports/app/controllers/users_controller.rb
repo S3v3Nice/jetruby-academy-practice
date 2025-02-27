897582974c822e6dabde520df9f3eb397b800a16
S3v3N <s3v3nicee@gmail.com>
2025-02-27 23:49:55 +0300

    lesson-7: Add `show` page for Users

--- lesson-7-rails-lab-reports/app/controllers/users_controller.rb
+++ lesson-7-rails-lab-reports/app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[edit update destroy]
+  before_action :set_user, only: %i[show edit update destroy]
 
   def index
     @previous_page = ["Lab Reports", lab_reports_path]
     @users = User.includes(:lab_reports).order(created_at: :desc).all
+  end
+
+  def show
   end
 
   def new
